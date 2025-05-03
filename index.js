59ad1a42b756b9e11f8ed9d3d8614ff79c2f9230
jose005111 <jose29032002@hotmail.com>
2025-05-03 20:59:34 +0100

    cors adicioando

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
+// Ativa o CORS para todas as rotas
+app.use(cors({
+    origin: '*', // exemplo: seu frontend em Vite
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
+  
+
+// Outras configurações
+app.use(express.json());
+
+const express = require('express');
 const app = express();
 require('dotenv').config();
 const delegadoRoutes = require('./routes/delegadoRoutes');
